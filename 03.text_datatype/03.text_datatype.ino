28da3b37f7d964547627ac1d275df4cf65f48d8b
Nilesh Patel <142680610+nilesh28A@users.noreply.github.com>
2023-08-30 23:48:56 +0530

    Add files via upload

---
+++ 03.text_datatype/03.text_datatype.ino
@@ -0,0 +1,16 @@
+
+char val1 = 65;
+char val2 = 66;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.print(val1);
+Serial.print(val2);
+// seek the value useing serial monitor 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ 05.array/05.array.ino
@@ -0,0 +1,13 @@
+String name[] = {"mango","apple","banana"};
+int var[5] = {10,11,12,13,14};
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.print(var[4]);
+Serial.print(name[1]);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
