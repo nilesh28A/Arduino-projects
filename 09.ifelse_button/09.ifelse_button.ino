0f3c72f051ceae4d1b4ec334ec4f9139839b8925
Nilesh Patel <142680610+nilesh28A@users.noreply.github.com>
2023-09-13 21:41:45 +0530

    Add files via upload

---
+++ 09.ifelse_button/09.ifelse_button.ino
@@ -0,0 +1,14 @@
+void setup() {
+  pinMode(2, INPUT_PULLUP);
+  pinMode(6, OUTPUT);
+}
+
+void loop() {
+  int val = digitalRead(2);
+  if(val == 00){
+    digitalWrite(6,HIGH);
+  }else{
+    digitalWrite(6,LOW);
+  }
+
+}
